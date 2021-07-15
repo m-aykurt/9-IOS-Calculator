a39242b243861308fdf6d83b1ead345e0f9eb3fc
m_aykurt <maykurt92@gmail.com>
2021-07-15 16:38:04 +0300

    first phase

---
+++ app.js
@@ -0,0 +1,27 @@
+const result = document.querySelector(".result");
+const num = document.querySelectorAll("span");
+const opr = document.querySelectorAll(".operator");
+const clear = document.querySelector(".clear")
+console.log(num);
+const equal = document.querySelector(".equal")
+// console.log(equal);
+
+clear.addEventListener("click",function(){
+    result.textContent = "0";
+    location.reload()
+})
+
+
+num.forEach(function(element){
+    element.onclick = function(e){
+        if (result.textContent=="0"){
+            result.textContent = "";
+        }
+        result.textContent += this.textContent
+    }
+    // console.log(result.textContent);
+})
+
+equal.addEventListener("click",function(){
+    result.textContent = eval(result.textContent)
+})
